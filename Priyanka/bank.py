6553682ac21fabc3e68da71c996dc4c9ab0c9fc4
Priyanka <priyankagavali2710@gmail.com>
2021-05-24 13:07:22 +0530

    commit by priyanka on Bank

---
+++ Priyanka/bank.py
@@ -0,0 +1,38 @@
+class Bank:
+
+    def __init__(self):
+        self.name = " "
+        self.age = " "
+        self.address = " "
+        self.phone = " "
+        self.branch = " "
+
+        self.result = 0
+        self.balance = 0
+
+    def details(self):
+
+        self.name = input("Name \n")
+        self.age = int(input("Age\n"))
+        self.address = input("Address\n")
+        self.phone = int(input(" phone\n"))
+        self.branch = input("Branch")
+
+    def deposit(self):
+        amount = float(input("Deposit "))
+        self.balance += amount
+        print(" \n Amount Deposited:", amount)
+
+    def withdraw(self):
+
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance >= amount:
+            self.balance -= amount
+            print(" \n You Withdraw:", amount)
+        else:
+            print(" \n Insufficient balance")
+
+    def display(self):
+
+        print(" \n Net Available Balance=", self.balance)
+
---
+++ Priyanka/bankdetails.py
@@ -0,0 +1,9 @@
+# collecting from bank.py
+from Priyanka.bank import Bank
+
+s = Bank()
+
+s.details()
+s.deposit()
+s.withdraw()
+s.display()
