fc3bde30014231e4a81eb1116100736e3d8eff9a
Gauravi-nikam <gauravinikam98@gmail.cojm>
2021-05-25 15:52:38 +0530

    commit by gauravi on bank.py

---
+++ gauravi/bankdetails.py
@@ -0,0 +1,18 @@
+# collecting from bank.py
+from gauravi.bank import Bank, Account
+
+#s = Bank()
+
+#s.details()
+#s.deposit()
+#s.withdraw()
+#s.display()
+
+
+
+
+# creating object of Account class
+A = Account()
+
+A.registration()
+A.login()
