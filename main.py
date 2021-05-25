7ee7a6d55e65c39879f22975d090c88888cd2e69
priyanka <priyankapawde1992@gmail.com>
2021-05-25 11:30:26 +0530

    commit by priyanka pawde on circle

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #calling for bankprg
-from first import bankprg
+#from first import bankprg
 
-b=bankprg.bank()
-b.get()
-b.deposit()
-b.withdraw()
+#b=bankprg.bank()
+#b.get()
+#b.deposit()
+#b.withdraw()
 
+from priya import circle
+obj = circle.cir()
+obj.get()
+obj.start()
+obj.out()
