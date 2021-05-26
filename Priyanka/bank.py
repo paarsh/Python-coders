4dd254284b2532e0fa4344f62dce59985b7c00a8
dinesh8262 <dineshgame35@gmail.com>
2021-05-26 09:56:57 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e553d52e43eed2f8588babfb079abf71936b5b25, reversing
    changes made to ced7cb69fdce37b3032e2b627f136da7fadcab85.

--- Priyanka/bank.py
+++ Priyanka/bank.py
@@ -1,7 +1,4 @@
-
-
-
-class bank:
+class Bank:
 
     def __init__(self):
         self.name = " "
@@ -11,7 +8,7 @@ class bank:
         self.branch = " "
 
         self.result = 0
-        self.balance = 20000
+        self.balance = 0
 
     def details(self):
 
@@ -38,33 +35,4 @@ class bank:
     def display(self):
 
         print(" \n Net Available Balance=", self.balance)
-
-
-    # inherited the bank class
-
-class Account(bank):
-    def __init__(self):
-            super().__init__()
-            self.username = " "
-            self.password = " "
-            self.login_username = " "
-            self.login_password = " "
-
-    def registration(self):
-        super().details()
-        self.username = input("Enter username\n")
-        self.password = int(input("Enter password\n"))
-
-    def login(self):
-            print(" login username:")
-            self.login_username = (input())
-
-            print("login password:")
-            self.login_password= int(input())
-
-            if (self.login_username == self.username) and (self.password == self.login_password):
-
-                print("Net Available Balance=", self.balance)
-            else:
-                print("can't login to account")
 
--- Priyanka/bankdetails.py
+++ Priyanka/bankdetails.py
@@ -1,15 +1,9 @@
 # collecting from bank.py
-from Priyanka.bank import bank, Account
+from Priyanka.bank import Bank
 
-#s = Bank()
+s = Bank()
 
-#s.details()
-#s.deposit()
-#s.withdraw()
-#s.display()
-
-# creating object of Account class
-A = Account()
-
-A.registration()
-A.login()
+s.details()
+s.deposit()
+s.withdraw()
+s.display()
